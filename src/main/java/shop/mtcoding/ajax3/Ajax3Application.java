d1c35b1a54c579f614335cf8082cacfa7bdc693f
aozp73 <aozp73@naver.com>
2023-02-02 20:14:59 +0900

    세팅 완료

---
+++ src/main/java/shop/mtcoding/ajax3/Ajax3Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.ajax3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ajax3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ajax3Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/ajax3/Ajax3ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.ajax3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ajax3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
